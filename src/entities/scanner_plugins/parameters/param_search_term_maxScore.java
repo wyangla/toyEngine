ac2747ab87f87fa3a27e5f1a04c6bfb499671fb6
wy2018 <wyglauk@gmail.com>
2018-11-10 11:42:06 +0000

    modify param_search_term_maxScore, not using clipping and removing, let the docSC keep growing as the scanning going, this is still fast, because of the counter extends LinkedHashMap, and sorted at each pUnit

--- src/entities/scanner_plugins/parameters/param_search_term_maxScore.java
+++ src/entities/scanner_plugins/parameters/param_search_term_maxScore.java
@@ -7,12 +7,12 @@ import utils.counter;
 public class param_search_term_maxScore {
 	// use this class to pass multiple data structures into the scanner
 	counter docUB;
-	counter tpKDocSC;	// changing
+	counter docSC;	// changing
 	int tpK;
 
-	public param_search_term_maxScore(scorer scr, counter docUpperBounds, counter topKDocumentScoreCounter, int topK){
+	public param_search_term_maxScore(scorer scr, counter docUpperBounds, counter documentScoreCounter, int topK){
 		docUB = docUpperBounds;
-		tpKDocSC = topKDocumentScoreCounter;
+		docSC = documentScoreCounter;
 		tpK = topK;
 	}
 	
@@ -23,32 +23,21 @@ public class param_search_term_maxScore {
 		
 		// in order to be efficient enough, here does not have the synchronisation,
 		// thus could be more than K scores
-		if(tpKDocSC.size() < tpK) {
-			tpKDocSC.increase(pUnit.docId, scr.cal_score(pUnit));
+		if(docSC.size() < tpK) {
+			docSC.increase(pUnit.docId, scr.cal_score(pUnit));
 		}else {
-			
-			// not always do clipping, only when it is too long
-			// faster than ensure topK
-			// also prevent sorting the longer and longer arrayList when not care about the over adding
-			if(tpKDocSC.size() > tpK) {	
-				synchronized(param_search_term_maxScore.class) {
-					tpKDocSC.remove_after_topK(tpK); // remove the docs lower than topK
-				}
-			}
-			
+			// not using clipping here, as could loose sub score contributions from terms 
 			Double docUpperBound = docUB.get(pUnit.docId);
-			String minScoreDocId = tpKDocSC.get_min_key();
-			Double minScore = tpKDocSC.get(minScoreDocId);
+			String topKthDocId = docSC.get_topKth_key(tpK);
+			Double topKthScore = docSC.get(topKthDocId);
 			
-			// only when the upper bound bigger than the current min score, calculate the actual value
 			// != null prevent new docs added during the ranking after the upper bound calculation
-			if(docUpperBound != null && docUpperBound > minScore) {
-				Double curScore = scr.cal_score(pUnit);
-				if(curScore > minScore) {
-					tpKDocSC.remove(minScoreDocId);		// here could remove the same id for multiple times
-					tpKDocSC.increase(pUnit.docId, curScore);	// could increase more than K
-					newlyAddedUnitId = pUnit.currentId;
-				}
+			// only when the upper bound bigger than the current min score, calculate the actual value
+			// not using remove, as could loose sub score
+			if(docUpperBound != null && docUpperBound > topKthScore) {
+				Double subScore = scr.cal_score(pUnit);		// score contribute by one term to the doc
+				docSC.increase(pUnit.docId, subScore);	// could increase more than K
+				newlyAddedUnitId = pUnit.currentId;
 			}
 		}
 		
