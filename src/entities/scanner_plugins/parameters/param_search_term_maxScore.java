7e59cb4f1daa1cf61b66a66649088d179ba3b06a
wy2018 <wyglauk@gmail.com>
2018-11-11 12:31:09 +0000

    use counter.safe_get in param_search_term_maxScore

--- src/entities/scanner_plugins/parameters/param_search_term_maxScore.java
+++ src/entities/scanner_plugins/parameters/param_search_term_maxScore.java
@@ -6,11 +6,13 @@ import utils.counter;
 
 public class param_search_term_maxScore {
 	// use this class to pass multiple data structures into the scanner
+	scorer scr;
 	counter docUB;
 	counter docSC;	// changing
 	int tpK;
 
-	public param_search_term_maxScore(scorer scr, counter docUpperBounds, counter documentScoreCounter, int topK){
+	public param_search_term_maxScore(scorer scorer, counter docUpperBounds, counter documentScoreCounter, int topK){
+		scr = scorer;
 		docUB = docUpperBounds;
 		docSC = documentScoreCounter;
 		tpK = topK;
@@ -19,18 +21,17 @@ public class param_search_term_maxScore {
 	// use the current docId replace the one with min score
 	public long try_to_score_add_doc(posting_unit pUnit){
 		long newlyAddedUnitId = -1L; 
-		scorer scr = scorer.getInstance();
 		
 		// in order to be efficient enough, here does not have the synchronisation,
 		// thus could be more than K scores
 		// if total number of docs is smaller than tpK, they will be all returned
-		if(docSC.size() < tpK) {
+		if(docSC.get_size() < tpK) {
 			docSC.increase(pUnit.docId, scr.cal_score(pUnit));
 		}else {
 			// not using clipping here, as could loose sub score contributions from terms 
 			Double docUpperBound = docUB.get(pUnit.docId);
 			String topKthDocId = docSC.get_topKth_key(tpK);
-			Double topKthScore = docSC.get(topKthDocId);
+			Double topKthScore = docSC.safe_get(topKthDocId);
 			
 			// != null prevent new docs added during the ranking after the upper bound calculation
 			// only when the upper bound bigger than the current min score, calculate the actual value
